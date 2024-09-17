d02e61b25f55fb96d08901b98fe5ccf1d724762c
ashutosh29599 <ashutosh29599@yahoo.co.in>
2024-09-17 15:36:41 -0500

    Refactor posts.views -- replace Create/Update/Delete API Views with a single ViewSet. Bump version to 0.5.0.

--- posts/views.py
+++ posts/views.py
@@ -1,51 +1,81 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import DestroyAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Post
 from .serializer import PostSerializer
 
 
-class PostCreateAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()  # type: ignore
+    serializer_class = PostSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            post = serializer.save(user=request.user)
-            return Response(PostSerializer(post).data, status=status.HTTP_201_CREATED)
+    def get_permissions(self):
+        if self.request.method in ['GET']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        username = self.request.query_params.get('username')
+        if username:
+            return Post.objects.filter(user__username=username)  # type: ignore
 
+        return Post.objects.all()  # type: ignore
 
-#
-# class PostCreateAPIView(CreateAPIView):
-#     queryset = Post.objects.all()  # type: ignore
-#     serializer_class = PostSerializer
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+# class PostCreateAPIView(APIView):
 #     permission_classes = [IsAuthenticated]
 #
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
-class PostUpdateAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, id=pk, user=request.user)
-        serializer = PostSerializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            post = serializer.save(user=request.user)
-            return Response(PostSerializer(post).data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             post = serializer.save(user=request.user)
+#             return Response(PostSerializer(post).data, status=status.HTTP_201_CREATED)
 #
-# class PostUpdateAPIView(UpdateAPIView):
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# #
+# # class PostCreateAPIView(CreateAPIView):
+# #     queryset = Post.objects.all()  # type: ignore
+# #     serializer_class = PostSerializer
+# #     permission_classes = [IsAuthenticated]
+# #
+# #     def perform_create(self, serializer):
+# #         serializer.save(user=self.request.user)
+#
+#
+# class PostUpdateAPIView(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def put(self, request, pk, *args, **kwargs):
+#         post = get_object_or_404(Post, id=pk, user=request.user)
+#         serializer = PostSerializer(post, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             post = serializer.save(user=request.user)
+#             return Response(PostSerializer(post).data, status=status.HTTP_200_OK)
+#
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# #
+# # class PostUpdateAPIView(UpdateAPIView):
+# #     queryset = Post.objects.all()  # type: ignore
+# #     serializer_class = PostSerializer
+# #     permission_classes = [IsAuthenticated]
+# #
+# #     def get_queryset(self):
+# #         return Post.objects.filter(user=self.request.user)  # type: ignore
+# #
+# #     def perform_update(self, serializer):
+# #         serializer.save(user=self.request.user)
+#
+#
+# class PostDeleteAPIView(DestroyAPIView):
 #     queryset = Post.objects.all()  # type: ignore
 #     serializer_class = PostSerializer
 #     permission_classes = [IsAuthenticated]
@@ -53,17 +83,5 @@ class PostUpdateAPIView(APIView):
 #     def get_queryset(self):
 #         return Post.objects.filter(user=self.request.user)  # type: ignore
 #
-#     def perform_update(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
-class PostDeleteAPIView(DestroyAPIView):
-    queryset = Post.objects.all()  # type: ignore
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Post.objects.filter(user=self.request.user)  # type: ignore
-
-    def perform_destroy(self, instance):
-        instance.delete()
+#     def perform_destroy(self, instance):
+#         instance.delete()
